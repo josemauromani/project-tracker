aea7b9496647879449e733639ae2dcb1d96de87f
Jose Mauro <jose.mani@oficinacerta.app>
2023-02-28 18:45:48 -0300

    adds components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideMenu />
+    </div>
+    <div class="column is-three-quarter">
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import SideMenu from "./components/SideMenu.vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { SideMenu },
 });
 </script>
 
---
+++ src/components/tracker/FormTracker.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulario para criacao de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa voce deseja iniciar?"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
